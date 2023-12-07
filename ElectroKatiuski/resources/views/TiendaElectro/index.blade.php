321e14a81a67ef85c402c31838e81f3d67f0ff6a
katiuskizzz <munozkatalinavalderrama@gmail.com>
2023-12-07 13:16:47 -0500

    el repositorio

---
+++ ElectroKatiuski/resources/views/TiendaElectro/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div style=" margin: 10% ; border: 1px solid black; align-items: center; height:600px;">
+    <div style= "margin-left:40% ; margin-right:40%;" >
+      <h1>
+        Inicio de Sesion
+    </h1>
+    <div>
+        <label for=""></label>
+        <input placeholder="Usuario" type="text" maxlength="20" minlenght="3">
+    </div>
+
+    <div>
+        <label for=""></label>
+        <input placeholder="contraseña" type="password" >
+    </div>
+    </div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
